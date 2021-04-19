b6721d14d3504ac5e52c8c2ef42fe713ec18381f
Adam Žák <adam.hrac@gmail.com>
2021-04-19 20:16:25 +0200

    First commit with users and User_Screen

---
+++ src/sk/stu/fiit/Project_Manager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit;
+
+/**
+ *
+ * @author adamh
+ */
+public class Project_Manager extends User {
+    private String type = "project_manajer";
+
+    public Project_Manager(String name, int age, String address, String picture) {
+        super(name, age, address, picture);
+    }
+}
---
+++ src/sk/stu/fiit/User.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit;
+
+/**
+ *
+ * @author adamh
+ */
+public class User {
+    private String name;
+    private int age;
+    private String address;
+    private String picture;
+    private String type = "user";
+
+    public User(String name, int age, String address, String picture) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.picture = picture;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
