5f5efcf33c872fe53bf6260563f4b58b365b3ecb
Adam Žák <adam.hrac@gmail.com>
2021-04-20 14:52:41 +0200

    Added pokusy na main_window

--- src/sk/stu/fiit/Main.java
+++ src/sk/stu/fiit/Main.java
@@ -1,5 +1,9 @@
 package sk.stu.fiit;
 
+import java.time.LocalTime;
+import java.util.Date;
+import sk.stu.fiit.Tasks.Task;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,5 +22,11 @@ public class Main {
         System.out.println("user name = " + user.getName());
         System.out.println("age name = " + user.getAge());
         System.out.println("user name = " + user.getAddress());
+        
+        
+        Task task = new Task(new Date(), new Date(), LocalTime.now(), LocalTime.now(), "toto je super brutalny Task, na ktorom sa seci tesime a pracujeme :)))))");
+        //Task task1 = new Task(new GregorianCalendar(), new Date(), new Date(), new Date(), "toto je super brutalny Task, na ktorom sa seci tesime a pracujeme :)))))");
+        System.out.println("Casiiiiiiik je: " + task.getDeadline());
+        
     }
 }
