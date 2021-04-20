6d845ea8f727bf425380c34e9baa9553aed65800
x-heg-las <heglas.pato@gmail.com>
2021-04-20 14:56:38 +0200

    GUI pre team

---
+++ src/sk/stu/fiit/renders/TeamTableCellRender.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.renders;
+
+/**
+ *
+ * @author patoh
+ */
+public class TeamTableCellRender {
+    
+}
