fa759e9107d862612c37e39b45e9eb06a0ed3900
Patrik Heglas <heglas.pato@gmail.com>
2021-04-27 21:14:51 +0200

    Opravene ukladanie projektov a task-ov

--- src/GUI/NewChart.java
+++ src/GUI/NewChart.java
@@ -13,38 +13,38 @@ public class NewChart extends ApplicationFrame {
    
    public NewChart( String title ) {
       super( title ); 
-      setContentPane(createDemoPanel( ));
+     // setContentPane(createDemoPanel( ));
    }
-   
-   private static PieDataset createDataset( ) {
-      DefaultPieDataset dataset = new DefaultPieDataset( );
-      dataset.setValue( "IPhone 5s" , new Double( 20 ) );  
-      dataset.setValue( "SamSung Grand" , new Double( 20 ) );   
-      dataset.setValue( "MotoG" , new Double( 40 ) );    
-      dataset.setValue( "Nokia Lumia" , new Double( 10 ) );  
-      return dataset;         
-   }
-   
-   private static JFreeChart createChart( PieDataset dataset ) {
-      JFreeChart chart = ChartFactory.createPieChart(      
-         "Mobile Sales",   // chart title 
-         dataset,          // data    
-         true,             // include legend   
-         false, 
-         false);
-
-      return chart;
-   }
-   
-   public static JPanel createDemoPanel( ) {
-      JFreeChart chart = createChart(createDataset( ) );  
-      return new ChartPanel( chart ); 
-   }
-
-   public static void main( String[ ] args ) {
-      NewChart demo = new NewChart( "Mobile Sales" );  
-      demo.setSize( 560 , 367 );    
-      RefineryUtilities.centerFrameOnScreen( demo );    
-      demo.setVisible( true ); 
-   }
+//   
+//   private static PieDataset createDataset( ) {
+//      DefaultPieDataset dataset = new DefaultPieDataset( );
+//      dataset.setValue( "IPhone 5s" , new Double( 20 ) );  
+//      dataset.setValue( "SamSung Grand" , new Double( 20 ) );   
+//      dataset.setValue( "MotoG" , new Double( 40 ) );    
+//      dataset.setValue( "Nokia Lumia" , new Double( 10 ) );  
+//      return dataset;         
+//   }
+//   
+//   private static JFreeChart createChart( PieDataset dataset ) {
+//      JFreeChart chart = ChartFactory.createPieChart(      
+//         "Mobile Sales",   // chart title 
+//         dataset,          // data    
+//         true,             // include legend   
+//         false, 
+//         false);
+//
+//      return chart;
+//   }
+//   
+//   public static JPanel createDemoPanel( ) {
+//      JFreeChart chart = createChart(createDataset( ) );  
+//      return new ChartPanel( chart ); 
+//   }
+//
+//   public static void main( String[ ] args ) {
+//      NewChart demo = new NewChart( "Mobile Sales" );  
+//      demo.setSize( 560 , 367 );    
+//      RefineryUtilities.centerFrameOnScreen( demo );    
+//      demo.setVisible( true ); 
+//   }
 }
